8e0ba804c4b6bca8d6a0e150284011ddeaa4d7c5
devender <devender.gollapally@gmail.com>
2016-04-10 17:06:45 -0700

    c

--- code/rest-service/src/main/java/com/gdr/puretawny/rest/GisService.java
+++ code/rest-service/src/main/java/com/gdr/puretawny/rest/GisService.java
@@ -1,9 +1,15 @@
 package com.gdr.puretawny.rest;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +19,25 @@ import com.gdr.puretawny.model.Point;
 @RestController
 @EnableAutoConfiguration
 public class GisService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GisService.class);
 
     @Autowired
-    private DbService dbService;
+    private DbService           dbService;
 
     @RequestMapping("/point/all")
     public List<Point> home() {
         return dbService.findAll();
+    }
+
+    @RequestMapping("/point/longitude/{longitude:.+}/latitude/{latitude:.+}")
+    public ResponseEntity lookup(@PathVariable double longitude, @PathVariable double latitude) {
+        LOGGER.debug("longitude {}, latitude {} ", longitude, latitude);
+
+        Optional<Point> o = dbService.findAt(latitude, longitude);
+        if (o.isPresent()) {
+            return new ResponseEntity<Point>(o.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
